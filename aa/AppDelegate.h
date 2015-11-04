da41d3083019c61afc0eb8d032f9f2a57e6b41e1
liyunqin <836497449@qq.com>
2015-11-04 11:11:44 +0800

    first commit

---
+++ aa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aa
+//
+//  Created by liyunqin on 15/11/4.
+//  Copyright (c) 2015年 liyunqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aa/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  aa
+//
+//  Created by liyunqin on 15/11/4.
+//  Copyright (c) 2015年 liyunqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic,copy) NSString *name;
+
+@end
+
